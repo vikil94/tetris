996d43ba0a74fb28f41bbb1e91a674a2d0b0fb74
Vikil Naik <vikilnaik37@gmail.com>
2019-08-26 18:42:32 -0400

    Creating the styling for the game by using styled-components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 
 const Cell = ({ type }) => (
-    <div>cell</div>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>cell</StyledCell>
 )
 
 export default Cell;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -12,17 +12,19 @@ const Tetris = () => {
 
 
     return (
-        <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                    <Display text="Score" />
-                    <Display text = "Rows" />
-                    <Display text = "Level" />
-                </div>
-                <StartButton />
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text="Score" />
+                        <Display text = "Rows" />
+                        <Display text = "Level" />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 };
 
