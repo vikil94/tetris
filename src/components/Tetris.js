5ec4a42985f250c4f545d5e80e18cb8916a46b11
Vikil Naik <vikilnaik37@gmail.com>
2019-08-26 18:18:40 -0400

    Created all the tetromino aka the shapes, using a function that randomizes which shape will generate on the screen

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createStage } from '../gameHelpers';
 
 //Components
 import Stage from './Stage';
@@ -12,7 +13,7 @@ const Tetris = () => {
 
     return (
         <div>
-            <Stage />
+            <Stage stage={createStage()} />
             <aside>
                 <div>
                     <Display text="Score" />
