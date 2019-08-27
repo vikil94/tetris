2281153e50ec98c94ecd46e6e8692599fdb43a6d
Vikil Naik <vikilnaik37@gmail.com>
2019-08-27 15:59:58 -0400

    created the logic for the useStage custom hook that will be used in the tetris.js component

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,9 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage()); // generates the initial stage for the game
+
+    useEffect(() => {
+        const updateStage = prevStage => {
+            // First flush the stage
+            const newStage = prevStage.map(row => 
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+            );
+
+            // Then draw the tetromino
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {  // checking to see which cells are occupied by the tetromino
+                    if (value !== 0) { // gives us the coordinate on the stage
+                        newStage[y + player.pos.y][x + player.pos.x] = [
+                            value,
+                            `${player.collided ? 'merged' : 'clear'}`,
+                        ]; 
+                    }
+                });
+            });
+
+            return newStage;
+        };
+
+        setStage(prev => updateStage(prev));
+    }, [player.collided, player.pos.x, player.pos.y, player.tetromino]); // have to specify them as dependencies beacuse we are using them in the useEffect
 
     return [stage, setStage];
 }
