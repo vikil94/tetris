5a08ecd9400760814785a75a95964c1078700085
Vikil Naik <vikilnaik37@gmail.com>
2019-09-02 14:25:04 -0400

    The rows now clear when they are full

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -5,8 +5,22 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage()); // generates the initial stage for the game
+    const [rowsCleared, setRowsCleared] = useState(0); 
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage => 
+            newStage.reduce((ack, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1);
+                    ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                return ack;
+            }, []);
+        
         const updateStage = prevStage => {
             // First flush the stage
             const newStage = prevStage.map(row => 
@@ -27,6 +41,7 @@ export const useStage = (player, resetPlayer) => {
             // Then check if we collided 
             if (player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
             }
             return newStage;
         };
@@ -34,5 +49,5 @@ export const useStage = (player, resetPlayer) => {
         setStage(prev => updateStage(prev));
     }, [player, resetPlayer]); // have to specify them as dependencies because we are using them in the useEffect
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 }
